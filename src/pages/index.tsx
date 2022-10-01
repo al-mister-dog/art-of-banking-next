d223b580ec77b92b927b7500c154d9648d6ed173
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-09-30 20:27:25 -0700

    remove exclamation point

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const IndexPage: NextPage = () => {
   return (
     <div style={{padding: 16}}>
-      <h1>Art of Banking!</h1>
+      <h1>Art of Banking</h1>
       <p>
         The world economy is made from the fabric of global finance. The fabric
         of global finance is an interlocking matrix of corporate balance sheets
